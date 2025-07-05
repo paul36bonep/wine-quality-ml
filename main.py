7f56b8fe41bd4b1f8fb14d468b82a191cccc781b
paul36bonep <tevesPG@gmail.com>
2025-07-05 13:51:08 +0800

    updated version

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import streamlit as st
-import joblib
+import pandas as pd
 import numpy as np
+import joblib
 
-# Load model and scaler
 model = joblib.load("model.pkl")
 scaler = joblib.load("scaler.pkl")
 
 st.title("Wine Quality Predictor")
 
-# Input fields
-st.header("Enter Chemical Attributes:")
-features = []
-fields = ['Fixed Acidity', 'Volatile Acidity', 'Citric Acid', 'Residual Sugar',
-          'Chlorides', 'Free Sulfur Dioxide', 'Total Sulfur Dioxide', 'Density',
-          'pH', 'Sulphates', 'Alcohol']
 
+st.header("Enter Chemical Attributes:")
+fields = [
+    'fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+    'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+    'pH', 'sulphates', 'alcohol'
+]
+
+user_input = []
 for field in fields:
     val = st.number_input(field, format="%.4f")
-    features.append(val)
+    user_input.append(val)
+
 
 if st.button("Predict Quality"):
-    x_input = scaler.transform([features])
-    prediction = model.predict(x_input)[0]
-    confidence = model.predict_proba(x_input)[0][prediction]
 
-    result = "Good" if prediction == 1 else "Not Good"
+    input_df = pd.DataFrame([user_input], columns=fields)
+    input_scaled = scaler.transform(input_df)
+    prediction = model.predict(input_scaled)[0]
+    confidence = model.predict_proba(input_scaled)[0][prediction]
+
+    result = "Good " if prediction == 1 else "Not Good"
     st.subheader(f"Prediction: {result}")
-    st.write(f"Confidence Score: {confidence:.2f}")
+    st.write(f"Confidence Score: **{confidence:.2f}**")
